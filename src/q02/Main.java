849b72cf46a39f51bae2f896ecf7bb16f4a1e4a6
Srashti katiyar <srastikatiyar2211@gmail.com>
2026-04-07 16:49:24 +0530

    Update Main.java

--- src/q02/Main.java
+++ src/q02/Main.java
@@ -4,70 +4,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[][] arr = new int[n][n];
-
-int top = 0, bottom = n - 1;
-int left = 0, right = n - 1;
-int num = 1;
-
-// Fill spiral
-while (top <= bottom && left <= right) {
-
-    // left → right
-    for (int i = left; i <= right; i++) {
-        arr[top][i] = num++;
-    }
-    top++;
-
-    // top → bottom
-    for (int i = top; i <= bottom; i++) {
-        arr[i][right] = num++;
-    }
-    right--;
-
-    // right → left
-    if (top <= bottom) {
-        for (int i = right; i >= left; i--) {
-            arr[bottom][i] = num++;
+        for (int i = 0; i < n; i++) {
+    int num = 1;
+    for (int j = 0; j <= i; j++) {
+        System.out.print(num);
+        
+        if (j < i) {
+            System.out.print(" ");
         }
-        bottom--;
-    }
-
-    // bottom → top
-    if (left <= right) {
-        for (int i = bottom; i >= top; i--) {
-            arr[i][left] = num++;
-        }
-        left++;
-    }
-}
-
-// Print matrix
-for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-        System.out.print(arr[i][j]);
-        if (j < n - 1) System.out.print(" ");
+        
+        num = num * (i - j) / (j + 1);
     }
     System.out.println();
 }
-
-// Diagonal sum
-int sum = 0;
-for (int i = 0; i < n; i++) {
-    sum += arr[i][i];
-}
-
-System.out.println("Diagonal: " + sum);
-        // TODO: Fill an N×N matrix in clockwise spiral order starting from 1
-        //       Print each row with values separated by single space
-        //       Then print: "Diagonal: X" where X = sum of primary diagonal (top-left to bottom-right)
+        // TODO: Print Pascal's Triangle for n rows
+        //       Values on each row separated by single space
+        //       No leading or trailing spaces
         //
-        // Input: 3
+        // Input: 5
         // Output:
-        // 1 2 3
-        // 8 9 4
-        // 7 6 5
-        // Diagonal: 15
+        // 1
+        // 1 1
+        // 1 2 1
+        // 1 3 3 1
+        // 1 4 6 4 1
 
     }
 }
