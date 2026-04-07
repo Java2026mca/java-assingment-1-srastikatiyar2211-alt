fbda3bb2cf5947d64f24d549fa9b7a14f9e0f81c
Srashti katiyar <srastikatiyar2211@gmail.com>
2026-04-07 16:06:02 +0530

    Update Main.java

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -20,6 +20,51 @@ public class Main {
         //  *   *
         //   * *
         //    *
+       for (int i = 1; i <= n; i++) {
 
+            // leading spaces
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+
+            if (i == 1) {
+                System.out.print("*");
+            } else {
+                System.out.print("*");
+
+                // inner spaces
+                for (int s = 1; s <= 2 * i - 3; s++) {
+                    System.out.print(" ");
+                }
+
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // Lower half
+        for (int i = n - 1; i >= 1; i--) {
+
+            // leading spaces
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+
+            if (i == 1) {
+                System.out.print("*");
+            } else {
+                System.out.print("*");
+
+                // inner spaces
+                for (int s = 1; s <= 2 * i - 3; s++) {
+                    System.out.print(" ");
+                }
+
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
     }
 }
